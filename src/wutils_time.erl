9fd623fc2d431b30aad16324ac05b5166d35ec0a
Nathaniel Waisbrot <code@waisbrot.net>
2015-06-04 17:01:42 -0400

    add a "shift" function to add and subtract times

--- src/wutils_time.erl
+++ src/wutils_time.erl
@@ -11,6 +11,7 @@
         ,datetime_to_epoch/1
         ,parse_iso/1
         ,format_iso/1
+        ,shift/3
         ]).
 
 -type hour() :: 0..23.
@@ -24,6 +25,9 @@
 
 %% utility functions for converting datetimes
 -define(GREGORIAN_SECONDS, 62167219200).
+-define(MILLISECONDS_PER_SECOND, 1000).
+-define(MILLISECONDS_PER_MINUTE, ?MILLISECONDS_PER_SECOND * 60).
+-define(MILLISECONDS_PER_HOUR, ?MILLISECONDS_PER_MINUTE * 60).
 
 -spec epoch_to_datetime(epoch_ms()) -> datetime().
 epoch_to_datetime(EpochMs) ->
@@ -52,6 +56,36 @@ format_iso({{Year, Month, Day}, {Hour, Minute, Second}}) ->
     Second1 = trunc(Second),
     MSecond = trunc((Second - Second1) * 1000),
     lists:flatten(io_lib:format("~4..0b-~2..0b-~2..0bT~2..0b:~2..0b:~2..0b.~3..0bZ", [Year, Month, Day, Hour, Minute, Second1, MSecond])).
+
+datetime_to_erlang_datetime({Date, {H,M,S}}) ->
+    SS = trunc(S),
+    MS = S - SS,
+    {{Date, {H,M,SS}}, MS}.
+erlang_datetime_to_datetime({{Date, {H,M,S}}, Ms}) ->
+    {Date, {H,M,S+Ms}}.
+
+-spec shift(datetime(), integer(), years|days|hours|minutes|seconds|milliseconds) -> datetime().
+shift({{YY,MM,DD},T}, Change, years) ->
+    {{YY + Change, MM, DD}, T};
+%shift({{YY,MM,DD},T}, Change, months) ->
+%    D = Change/12,
+%    Dy = trunc(D),
+%    Dm = round((D - Dy)*12),  %% "round" to avoid rounding errors. Should be trunc() if I knew that was safe
+%    {{YY + Dy, MM + Dm, DD}, T};
+shift({Date, Time}, Change, days) ->
+    DateShifted = calendar:gregorian_days_to_date(calendar:date_to_gregorian_days(Date) + Change),
+    {DateShifted, Time};
+shift(Datetime, Change, hours) ->
+    shift(Datetime, Change * ?MILLISECONDS_PER_HOUR, milliseconds);
+shift(Datetime, Change, minutes) ->
+    shift(Datetime, Change * ?MILLISECONDS_PER_MINUTE, milliseconds);
+shift(Datetime, Change, seconds) ->
+    shift(Datetime, Change * ?MILLISECONDS_PER_SECOND, milliseconds);
+shift(Datetime, Change, milliseconds) ->
+    Epoch = datetime_to_epoch(Datetime),
+    ShiftedEpoch = Epoch + Change,
+    epoch_to_datetime(ShiftedEpoch).
+
 
 
 %%% EUNIT
@@ -84,6 +118,18 @@ format_iso_test_() ->
      ?_assertEqual("2015-05-07T15:40:48.347Z", format_iso({{2015,5,7},{15,40,48.347}}))
     ,?_assertEqual("1970-01-01T00:00:00.000Z", format_iso({{1970,1,1},{0,0,0.0}}))
     ,?_assertEqual({{2015,5,7},{15,40,48.347}}, parse_iso(format_iso({{2015,5,7},{15,40,48.347}})))
+    ].
+
+shift_test_() ->
+    [
+     ?_assertEqual({{2016,1,1},{0,0,0}}, shift({{2015,1,1},{0,0,0}}, 1, years))
+    ,?_assertEqual({{2014,1,1},{0,0,0}}, shift({{2015,1,1},{0,0,0}}, -1, years))
+%    ,?_assertEqual({{2015,2,1},{0,0,0}}, shift({{2015,1,1},{0,0,0}}, 1, months))
+%    ,?_assertEqual({{2014,12,1},{0,0,0}}, shift({{2015,1,1},{0,0,0}}, -1, months))
+    ,?_assertEqual({{2015,1,2},{0,0,0}}, shift({{2015,1,1},{0,0,0}}, 1, days))
+    ,?_assertEqual({{2014,12,31},{0,0,0}}, shift({{2015,1,1},{0,0,0}}, -1, days))
+    ,?_assertEqual({{2015,1,1},{1,0,0.0}}, shift({{2015,1,1},{0,0,0}}, 1, hours))
+    ,?_assertEqual({{2014,12,31},{23,0,0.0}}, shift({{2015,1,1},{0,0,0}}, -1, hours))
     ].
 
 -endif.
